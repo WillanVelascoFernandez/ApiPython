c4680b4936836d742d1af4091b881085aabfa124
Ari_PG <arianepozogalvis@gmail.com>
2023-12-20 18:47:56 -0400

    adicion de busqueda por proveedor

--- servicio_orm.py
+++ servicio_orm.py
@@ -1,19 +1,17 @@
-import webbrowser
 from flask import Flask, jsonify
-import flask
 from flask_restful import Resource, Api, request
-import flask_restful
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask("Practica APIs Python")
+app = Flask("LottoApi")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mariadb://root:1234@localhost:3306/api'
 db = SQLAlchemy(app)
 api = Api(app)
 
 class ModeloLoteria(db.Model):
-    __tablename__ = 'loteria'
+    __tablename__ = 'lottoapi'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     fecha = db.Column(db.String(50))
+    proveedor = db.Column(db.String(50))
     nombreLoteria = db.Column(db.String(50))
     numeros = db.Column(db.String(10))
 
@@ -21,7 +19,7 @@ class ModeloLoteria(db.Model):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
     def __repr__(self):
-        return f'Historial: id={self.id}, fecha={self.fecha}, nombreLoteria={self.nombreLoteria}, numeros={self.numeros}'
+        return f'Historial: id={self.id}, fecha={self.fecha}, proveedor={self.proveedor},, nombreLoteria={self.nombreLoteria}, numeros={self.numeros}'
 
 class ListarLoteria(Resource):
     def get(self):
@@ -54,6 +52,17 @@ class ListarLoteriaPorNombre(Resource):
             return jsonify(result)
         else:
             return {"message": "No se encontró el elemento"}, 404
+        
+class ListarLoteriaPorProveedor(Resource):
+    def get(self, proveedor=None):
+        # items = ModeloLoteria.query.filter(ModeloLoteria.id == 1).first()
+        items = ModeloLoteria.query.filter(ModeloLoteria.proveedor==proveedor)
+        if items:
+            result = [item.as_dict() for item in items]
+            # return jsonify(item.as_dict())
+            return jsonify(result)
+        else:
+            return {"message": "No se encontró el elemento"}, 404
 
 with app.app_context():
     db.create_all()
@@ -65,17 +74,15 @@ with app.app_context():
     # db.session.add(loteria2)
     # db.session.commit()
 
-        # Agrega 20 loterías de Bolivia como datos de ejemplo
+        # Agrega loterías de Bolivia como datos de ejemplo
     loterias_bolivia = [
-        {'fecha': '2023-01-01', 'nombreLoteria': 'Loteria1', 'numeros': '1,2,5,5,9'},
-        {'fecha': '2023-01-01', 'nombreLoteria': 'Loteria2', 'numeros': '6,2,9,3,1'},
-        {'fecha': '2023-01-01', 'nombreLoteria': 'Loteria3', 'numeros': '7,3,7,2,3'},
-        {'fecha': '2023-01-02', 'nombreLoteria': 'Loteria1', 'numeros': '6,5,3,7,4'},
-        {'fecha': '2023-01-02', 'nombreLoteria': 'Loteria2', 'numeros': '8,7,1,9,9'},
-        {'fecha': '2023-01-02', 'nombreLoteria': 'Loteria3', 'numeros': '6,2,2,4,0'},
-        {'fecha': '2023-01-03', 'nombreLoteria': 'Loteria1', 'numeros': '9,8,5,8,6'},
-        {'fecha': '2023-01-03', 'nombreLoteria': 'Loteria2', 'numeros': '0,9,8,2,0'},
-        {'fecha': '2023-01-03', 'nombreLoteria': 'Loteria3', 'numeros': '5,0,7,9,2'},
+        {'fecha': '2023-01-01', 'proveedor': 'Banco', 'nombreLoteria': 'Macro Cuenta', 'numeros': '1,2,5,5,9'},
+        {'fecha': '2023-01-01', 'proveedor': 'Loteria', 'nombreLoteria': 'Nacional', 'numeros': '6,2,9,3,1'},
+        {'fecha': '2023-01-01', 'proveedor': 'Banco', 'nombreLoteria': 'Fabulosa', 'numeros': '7,3,7,2,3'},
+        {'fecha': '2023-01-02', 'proveedor': 'Loteria', 'nombreLoteria': 'Navideña', 'numeros': '6,5,3,7,4'},
+        {'fecha': '2023-01-02', 'proveedor': 'Banco', 'nombreLoteria': 'GanaDoble', 'numeros': '8,7,1,9,9'},
+        {'fecha': '2023-01-02', 'proveedor': 'Loteria', 'nombreLoteria': 'Semanal', 'numeros': '6,2,2,4,0'},
+        {'fecha': '2023-01-03', 'proveedor': 'Loteria', 'nombreLoteria': 'Diaria', 'numeros': '0,9,8,2,0'},
     ]
 
     for loteria_data in loterias_bolivia:
@@ -85,6 +92,7 @@ with app.app_context():
 
 api.add_resource(ListarLoteria, '/api')
 api.add_resource(ListarLoteriaPorNombre, '/api/nombre/<string:nombreLoteria>')
+api.add_resource(ListarLoteriaPorProveedor, '/api/proveedor/<string:proveedor>')
 
 if __name__ == "__main__":
     app.run(host='172.25.112.1', port=5055,debug=True)
